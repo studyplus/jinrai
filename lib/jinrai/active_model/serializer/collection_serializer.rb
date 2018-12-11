0c4b2ba16ac711042f96a6e1681ad108421ac01f
atomiyama <akifumi.tomiyama@studyplus.jp>
2018-12-11 16:03:45 +0900

    initial commit

--- lib/jinrai/active_model/serializer/collection_serializer.rb
+++
@@ -1,9 +0,0 @@
-module ActiveModel
-  class Serializer
-    class CollectionSerializer
-      def paginated?
-        ActiveModelSerializers.config.jsonapi_pagination_links_enabled && object.try(:cursored?)
-      end
-    end
-  end
-end
