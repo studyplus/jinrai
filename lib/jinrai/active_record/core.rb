93894e3f5b7a357a6119180a4095bc551262bc3f
atomiyama <tomisuker16@gmail.com>
2018-12-21 12:50:39 +0900

    fix oneline namespacing

--- lib/jinrai/active_record/core.rb
+++ lib/jinrai/active_record/core.rb
@@ -1,20 +1,22 @@
 require 'jinrai/active_record/finder_methods'
 
-module Jinrai::ActiveRecord
-  module Core
-    extend ActiveSupport::Concern
+module Jinrai
+  module ActiveRecord
+    module Core
+      extend ActiveSupport::Concern
 
 
-    module ClassMethods
-      def inherited(kls)
-        super
-        kls.send(:include, Jinrai::ActiveRecord::FinderMethods) if kls.superclass == ::ActiveRecord::Base
+      module ClassMethods
+        def inherited(kls)
+          super
+          kls.send(:include, Jinrai::ActiveRecord::FinderMethods) if kls.superclass == ::ActiveRecord::Base
+        end
       end
-    end
 
-    included do
-      descendants.each do |kls|
-        kls.send(:include, Jinrai::ActiveRecord::FinderMethods) if kls.superclass == ::ActiveRecord::Base
+      included do
+        descendants.each do |kls|
+          kls.send(:include, Jinrai::ActiveRecord::FinderMethods) if kls.superclass == ::ActiveRecord::Base
+        end
       end
     end
   end
